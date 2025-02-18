aa75b03d90d15bd318dee88a5260519049d712bb
korhoneCom <103757909+korhoneCom@users.noreply.github.com>
2025-02-18 15:39:10 +0200

    Update m3u-parse.php

--- m3u-parse.php
+++ m3u-parse.php
@@ -3,7 +3,7 @@ $url = $_GET['url'];
 $password = $_GET['password'];
 $type = $_GET['type'];
 $output = $_GET['output'];
-$url = $_GET['url']+"&password={}&type={$type}&output={$output}";
+$url = $_GET['url']+"&password={$password}&type={$type}&output={$output}";
 $m3u = file_get_contents($url);
 print($url);
 print($m3u);
