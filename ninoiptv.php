5492f87e6f43a5dfe89acb6e436240c65e91cdd0
korhoneCom <103757909+korhoneCom@users.noreply.github.com>
2025-02-18 13:34:21 +0200

    Update ninoiptv.php

--- ninoiptv.php
+++ ninoiptv.php
@@ -12,7 +12,7 @@ for($i=1;$i<count($nino);$i++)
     for($u=1;$u<count($urls);$u++)
       {
         $plainUrls = explode("\n",$urls[$u]);
-        print("http"+$plainUrls[0]+"<br>");
+        print("http {$plainUrls[0]}<br>");
       }
   }
 ?>
