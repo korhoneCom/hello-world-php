29c1ef107c1d232c907548b05bf30a3735506a59
korhoneCom <103757909+korhoneCom@users.noreply.github.com>
2025-02-18 13:20:15 +0200

    Update ninoiptv.php

--- ninoiptv.php
+++ ninoiptv.php
@@ -1,5 +1,13 @@
 <?php
 $url = "https://www.ninoiptv.com";
-$nino = file_get_contents($url);
-print($nino);
+$ninoiptv = file_get_contents($url);
+$nino = explode("entry-title-link",$ninoiptv);
+for($i=1;$i<count($nino);$i++)
+  {
+    $ninoUrl = explode("'",$nino[$i]);
+    $pageUrl = $ninoUrl[2];
+    $ninoPage = file_get_contents($pageUrl);
+    $page = explode("post-body",$ninoPage);
+    print($page[48]);
+  }
 ?>
